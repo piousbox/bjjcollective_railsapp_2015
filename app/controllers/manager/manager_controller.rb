3fc91e203a6d1a1fb7079387488dc015f768a6f2
Victor Piousbox <piousbox@gmail.com>
2015-11-22 07:59:12 -0800

    htaccess of manager

--- app/controllers/manager/manager_controller.rb
+++ app/controllers/manager/manager_controller.rb
@@ -1,3 +1,14 @@
 
 class Manager::ManagerController < ApplicationController
+
+  before_filter :authenticate
+  
+  protected
+  
+  def authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      username == "bjjc" && password == "technique-conquers-all"
+    end
+  end
+  
 end
