8bb28a78e5f2a32ae3655343968c52d458bb0fb0
Victor Piousbox <piousbox@gmail.com>
2015-11-29 08:59:25 -0800

    / redirect to fg namespace

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,14 +2,17 @@
 class WelcomeController < ApplicationController
 
   def home
-    respond_to do |format|
-      format.html do
-        render
-      end
-      format.json do
-        render :json => { :status => :ok }
-      end
-    end
+    redirect_to fg_root_path and return
+    
+    # respond_to do |format|
+    #   format.html do
+    #     render
+    #   end
+    #   format.json do
+    #     render :json => { :status => :ok }
+    #   end
+    # end
+    
   end
 
 end
---
+++ spec/requests/welcome_spec.rb
@@ -0,0 +1,10 @@
+require "spec_helper"
+
+describe "external redirection" do
+
+  it 'redirects / to /fighters_guild' do
+    get "/"
+    response.should redirect_to( "/fighters_guild" )
+  end
+
+end
