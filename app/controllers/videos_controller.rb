81500352eb11468089a73876bb60c8e4eaf95adf
Victor Piousbox <piousbox@gmail.com>
2015-12-18 13:37:01 -0800

    working on videos/show

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -3,6 +3,12 @@ class VideosController < ApplicationController
 
   def show
     @video = Video.find( params[:id] )
+    c = @video.category
+    @categories_list = [ c ]
+    while c.category
+      @categories_list.unshift c.category
+      c = c.category
+    end
   end
 
 end
