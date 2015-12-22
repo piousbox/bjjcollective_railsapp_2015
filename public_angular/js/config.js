d80b328a6bb9f94233c1444fff2621395401585e
Victor Piousbox <piousbox@gmail.com>
2015-12-21 17:39:15 -0800

    NOT WORKING - merging multi virtual sites to make angular at /technique.

--- public_angular/js/config.js
+++ public_angular/js/config.js
@@ -8,7 +8,7 @@ var myApp = angular.module('myApp');
 
 // myApp.value('Config', { api_endpoint: 'http://staging.api.travel-guide.mobi/api' });
 
-myApp.value('Config', { api_endpoint: 'http://bjjc-angular.local/api' });
+myApp.value('Config', { api_endpoint: '/api' });
 
 
 
