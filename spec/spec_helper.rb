df776d2e12af5b7def3a4fe455587520c10b9329
Victor Piousbox <piousbox@gmail.com>
2015-12-30 14:59:30 -0800

    first spec for lib/categories_tasks

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -154,3 +154,17 @@ def setup_videos
   @video                = FactoryGirl.create :video
 end
 
+# Metaprogrammatical magic to temporarily expose
+# a Class' privates (methods).
+class Class
+  def publicize_methods
+    saved_private_instance_methods = self.private_instance_methods
+    self.class_eval { public *saved_private_instance_methods }
+    yield
+    self.class_eval { private *saved_private_instance_methods }
+  end
+end
+
+
+
+
