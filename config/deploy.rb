b7f146906580c7d899c0757d29573b0785f29797
piousbox <piousbox@gmail.com>
2017-11-08 10:34:55 -0800

    cap run for vm succeeded

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,39 +1,31 @@
-# config valid for current version and patch releases of Capistrano
-lock "~> 3.10.0"
+lock "3.10.0"
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :application, "bjjcapi"
+set :repo_url, "git@github.com:piousbox/bjjcollective_railsapp_2015.git"
 
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
-
-# Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
-
-# Default value for :format is :airbrussh.
-# set :format, :airbrussh
-
-# You can configure the Airbrussh format using :format_options.
-# These are the defaults.
-# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
-
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for :linked_files is []
-# append :linked_files, "config/database.yml", "config/secrets.yml"
-
-# Default value for linked_dirs is []
+append :linked_files, "config/initializers/00_s3.rb", "config/mongoid.yml"
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+# set :linked_files, %w( config/initializers/00_s3.rb config/mongoid.yml )
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
-# Default value for local_user is ENV['USER']
-# set :local_user, -> { `git config user.name`.chomp }
+set :deploy_via, :remote_cache
 
-# Default value for keep_releases is 5
-# set :keep_releases, 5
+namespace :deploy do
+  task :bundle do
+    on roles(:web) do
+      execute "cd #{fetch(:deploy_to)}/current && sudo /home/#{fetch(:app_user)}/.rbenv/versions/2.3.1/bin/bundle --path #{fetch(:deploy_to)}/vendor/bundle"
+    end
+  end
 
-# Uncomment the following to require manually verifying the host key before first deploy.
-# set :ssh_options, verify_host_key: :secure
+  task :restart_nginx do
+    on roles(:web) do
+      execute "sudo systemctl restart nginx.service"
+    end
+  end
+end
+
+after "deploy:published", "bundle"
+after "deploy:published", "restart_nginx"
+
